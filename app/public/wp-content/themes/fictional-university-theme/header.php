13fdf1bc772c14250254d90098a65a4c27ae1d3e
Tony Aldea <tonythecoder@outlook.com>
2024-12-27 18:21:55 -0500

    Correcting and testing GitHub connection from VS Code.

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
-    <title>Document</title>
+    <title>Documents</title>
 
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
